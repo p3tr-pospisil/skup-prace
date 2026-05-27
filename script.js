c8e0e92857a1ee20cca5e505905b3230d86a5962
Skerett <filip.gasparik@gmail.com>
2026-05-27 18:26:13 +0200

    feat: Logic prep

--- script.js
+++ script.js
@@ -16,10 +16,75 @@ function shuffle(array) {
   return array; // Returning the shuffled array
 }
 
+// Data pro naše kvízy
+const questionsData = [
+  { 
+    text: "Jak je těžký tank?", 
+    answers: ["10 tun", "60 tun", "200 tun", "5 tun"], 
+    rightAnswer: 1 // index
+  },
+  // TODO: Doplnit data
+]
+
+// Naše hlavní třída pro kvíz
 class Quiz {
     constructor(questions) {
         this.questions = questions;
         this.currentQuestion = 0;
-        this.answers = [];
+        this.score = 0;
     }
+
+    // Vratí aktualní otázku
+    getCurrentQuestion(){
+      // TODO
+    }
+
+    // Uloží odpověď a posune kvíz dál
+    saveAnswer(currentIndex){
+      // TODO: porovnání odpovědí a případný inkrement skore
+      // inkrement currentIndex
+    }
+
+    // Zjistí jestli jsme na konci kvízu
+    isQuizActive(){
+      // TODO
+    }
+}
+
+// TODO: Propojení s HTML
+// hint: question, answer, quizContainter
+
+// TODO: napsat funkci pro vykreslení otázky a kontrolu konce
+function printQuestion(quiz){
+  // Kontrola jestli jsme nakonci a vypsání Konec Kvízu! + score
+  // Vem aktualní otázku a přepiš text otázky v html
+
+  // Přepiš text odpovědí 
+  array.forEach(element, index => {
+    
+  });
+}
+
+function handleClick(event, quiz){
+  const vybranyIndex = parseInt(udalost.target.dataset.index);
+
+  quiz.saveAnswer(vybranyIndex);
+  
+  printQuestion(quiz);
+}
+
+// Kód se musí spustit pouze pokud jsme na quiz.html
+if (/* existuje kvízová otázka a odpovědi */){
+
+  // TODO
+  // Vytvoř nový kvíz
+  // Posluchače pro každou odpověď
+  // hint: 
+  // NazevOdpovedi.forEach(element => {
+  // element.PridatPosluchače('click', (event) => {
+  //    handleClick(event, newQuiz)
+  //    });
+  // });
+  // Vykresli otázku
+
 }
